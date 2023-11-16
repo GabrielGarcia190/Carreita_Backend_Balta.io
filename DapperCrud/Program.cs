1b2040fbc4c1eba769f9c8bdc7fc29b02f1a0803
Gabriel Garcia <gabriel.alves@inovafarma.com.br>
2023-11-16 10:53:03 -0300

    feat: Adicionado função de cadastrar (Create)

--- DapperCrud/Program.cs
+++ DapperCrud/Program.cs
@@ -6,9 +6,10 @@ using Microsoft.Data.SqlClient;
 Console.WriteLine("=====================CRUD DAPPER ============================");
 Console.WriteLine("SELECIONE QUAL OPERAÇÃO DESEJA FAZER");
 Console.WriteLine("1: CADASTRAR CURSOS");
-Console.WriteLine("2: LISTAR CURSOS");
-Console.WriteLine("3: EDITAR CURSOS");
-Console.WriteLine("4: ELINIMAR CURSO");
+Console.WriteLine("2: LISTAR TODOS OS CURSOS");
+Console.WriteLine("3: PROCURAR CURSO POR NOME");
+Console.WriteLine("4: EDITAR CURSOS");
+Console.WriteLine("5: ELINIMAR CURSO");
 var escolha = Console.ReadLine();
 
 switch (escolha)
@@ -47,6 +48,54 @@ switch (escolha)
 
         break;
 
+    case "2":
+        try
+        {
+            await using (var connection = new SqlConnection(connectionString._connectionString))
+            {
+
+                var categories = await connection.QueryAsync("SELECT [Id], [Title], [Slug], [Description] FROM [Category]");
+                if (categories == null)
+                {
+                    Console.WriteLine("Nenhum resultado encontrado");
+                    break;
+                }
+                foreach (var category in categories)
+                    Console.WriteLine("Titulo do Curso" + category.Title);
+            }
+        }
+        catch (Exception ex)
+        {
+
+            Console.WriteLine(ex.Message);
+        }
+        break;
+
+    case "3":
+        try
+        {
+            Console.WriteLine("Insira o titulo do Curso: ");
+            var _nomeCurso = Console.ReadLine();
+
+            Console.WriteLine();
+            await using (var connection = new SqlConnection(connectionString._connectionString))
+            {
+                var categories = await connection.QueryFirstOrDefaultAsync("SELECT [Id], [Title], [Slug], [Description] FROM [Category] WHERE [Title]=@Title", new { Title = _nomeCurso });
+                if (categories == null)
+                {
+                    System.Console.WriteLine("Nenhum resultado encontrado");
+                    break;
+                }
+
+                Console.WriteLine("Titulo do Curso: " + categories.Title);
+            }
+        }
+        catch (Exception ex)
+        {
+
+            Console.WriteLine(ex.Message);
+        }
+        break;
     default:
         Console.WriteLine("Escolha uma opção válida");
         break;
