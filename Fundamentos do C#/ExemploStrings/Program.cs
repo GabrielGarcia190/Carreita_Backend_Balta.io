7f7148f6994fea227dd6dd6eb83f44f772231e3e
Gabriel Garcia <gabriel.alves@inovafarma.com.br>
2023-11-17 14:54:50 -0300

    Finaliando o modulo Strings

---
+++ Fundamentos do C#/ExemploStrings/Program.cs
@@ -0,0 +1,43 @@
+﻿namespace ExemploStrings
+{
+    class Program
+    {
+
+        static void Main(string[] args)
+        {
+            var texto = "Esse texto é um teste";
+
+            Console.WriteLine(texto.IndexOf("é"));
+            Console.WriteLine(texto.LastIndexOf("s"));
+        }
+        //EXEMPLO COMEÇA-COM E TERMINA-COM
+        // static void Main(string[] args)
+        // {
+        //     var texto = "Este texto testa";
+        //     Console.WriteLine(texto.StartsWith("Este"));
+        //     Console.WriteLine(texto.StartsWith("Este  "));
+
+        //     Console.WriteLine(texto.EndsWith("testa  "));
+        //     Console.WriteLine(texto.EndsWith("testa"));
+        //     Console.WriteLine(texto.EndsWith("Testa", StringComparison.OrdinalIgnoreCase));
+        // }
+        //COMPARAÇÃO DE TEXTO
+        // static void Main(string[] args)
+        // {
+        //     Console.Clear();
+        //     var texto = "Testando";
+        //     Console.WriteLine(texto.CompareTo("Testando"));
+        //     Console.WriteLine(texto.CompareTo("testando"));
+
+        //     Console.WriteLine(texto.Contains("T"));
+        //     Console.WriteLine(texto.Contains("E", StringComparison.OrdinalIgnoreCase));
+        // }
+        //EXEMPLO GUID
+        // static void Main(string[] args)
+        // {
+        //     var id = Guid.NewGuid();
+
+        //     Console.WriteLine(id);
+        // }
+    }
+}
