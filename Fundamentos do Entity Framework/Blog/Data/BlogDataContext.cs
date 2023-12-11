6d975cd328ec06b78c1e2e05335d91993c761bc7
Gabriel Garcia <gabriel.alves@inovafarma.com.br>
2023-12-11 09:34:50 -0300

    Finalizando: introdução ao Entity Framework

---
+++ Fundamentos do Entity Framework/Blog/Data/BlogDataContext.cs
@@ -0,0 +1,20 @@
+using Blog.Models;
+using BLog.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace Blog.Data
+{
+    public class BlogDataContext : DbContext
+    {
+        public DbSet<Category> Categories { get; set; }
+        public DbSet<Post> Posts { get; set; }
+        // public DbSet<PostTag> PostTag { get; set; }
+        public DbSet<Role> Roles { get; set; }
+        public DbSet<Tag> Tags { get; set; }
+        public DbSet<User> Users { get; set; }
+        // public DbSet<UserRole> UserRoles { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder options)
+        => options.UseSqlServer(@"Data Source=GARCIA-PC\SQL2022 ;Initial Catalog=Blog;User ID=sa; pwd=inovafarmaI;Encrypt=False;");
+    }
+}
---
+++ Fundamentos do Entity Framework/Blog/Program.cs
@@ -0,0 +1,38 @@
+﻿using Blog.Data;
+using Blog.Models;
+
+namespace Blog
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            using (var context = new BlogDataContext())
+            {
+                //CREATE
+                // var tag = new Tag { Name = "ASP.NET", Slug = "aspnet" };
+                // context.Tags.Add(tag);
+                // context.SaveChanges();
+
+                //UPDATE
+                // var tag = context.Tags.FirstOrDefault(x => x.Id == 2);
+                // tag.Name = ".NET";
+                // tag.Slug = "dotnet";
+                // context.Update(tag);
+                // context.SaveChanges();
+
+                //DELETE
+                // var tag = context.Tags.FirstOrDefault(x => x.Id == 2);
+                // context.Remove(tag);
+                // context.SaveChanges();
+
+                var tags = context.Tags.ToList();
+
+                foreach (var tag in tags)
+                {
+                    Console.WriteLine(tag.Name);
+                }
+            }
+        }
+    }
+}
